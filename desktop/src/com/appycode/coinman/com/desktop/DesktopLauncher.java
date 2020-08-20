67192426b324de5a9c4fffcb985316a216aae63b
Jas-96 <30918742+Jas-96@users.noreply.github.com>
2020-08-20 15:40:08 +0530

    Initial commit

---
+++ desktop/src/com/appycode/coinman/com/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.appycode.coinman.com.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.appycode.coinman.com.CoinMan;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoinMan(), config);
+	}
+}
